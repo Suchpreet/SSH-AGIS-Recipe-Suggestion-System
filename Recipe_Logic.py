c35e9f6e79d3f40ddab4a50a3469636b1c0c2001
Hackerman1234557 <imisioluwalawal@gmail.com>
2024-12-06 22:40:31 +0000

    Adding the recipe class to Recipe_Logic.py, as well as the beginnings of the match ingredients function

--- Recipe_Logic.py
+++ Recipe_Logic.py
@@ -1,6 +1,17 @@
 import sqlite3 as lite
 import csv
 
+class Recipe:
+    def __init__(self, name, ingredients, instructions, meal_type, veg_status, prep_time):
+        self.name = name
+        self.ingredients = ingredients  # list of ingredients in the recipe
+        self.instructions = instructions  # list of instructions
+        self.meal_type = meal_type  # string telling you what type of meal this is
+        self.veg_status = veg_status  # bool flag showing whether recipe is vegan or not
+        self.prep_time = prep_time  # int telling you how long the recipe will take to make in minutes
+def match_ingredients(ingredients, recipes):
+    ingredients_list = ingredients.split(',')
+
 recipesDatabase = lite.connect('foodDatabase.db')
 repCur = recipesDatabase.cursor()
 
